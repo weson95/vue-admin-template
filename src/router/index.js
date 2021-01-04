bbe8f0661e024b519618ccd780445c2d4ad86c7f
Weson <weson95@163.com>
2021-01-04 15:27:28 +0800

    add：切换页面进度条

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routers'
+import NProgress from 'nprogress'
+import './nprogress.less'
+import { setTitle } from '@/util'
+
+NProgress.inc(0.2)
+NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
 
 Vue.use(Router)
 const router = new Router({
   routes,
   mode: 'history'
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(to => {
+  setTitle(to, router.app)
+  NProgress.done()
+  window.scrollTo(0, 0)
 })
 
 export default router
